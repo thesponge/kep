66fedd2edb627ffa8390761293bac59bab40ceba
georgiana-b <bere.georgi@gmail.com>
2015-03-05 19:54:11 +0200

    Added wannabe login rounte

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -1,12 +1,13 @@
 import Ember from 'ember';
 import config from './config/environment';
 
+
 var Router = Ember.Router.extend({
   location: config.locationType
 });
 
-Router.map(function() {
-  this.route('login', {path: '/login'});
+App.Router.map(function() {
+  this.route('login');
 });
 
 export default Router;
