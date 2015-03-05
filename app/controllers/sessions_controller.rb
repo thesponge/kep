0aaa8ff9d41a654156ddc8f680cedc3fa5dc7397
Victor Nițu <victor@thesponge.eu>
2015-03-05 20:33:35 +0200

    Further Devise configuration for Ember

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,28 @@
 class SessionsController < Devise::SessionsController
-  respond_to :html, :json
-
+  # respond_to :html, :json
+  # 
+  # def create
+  #   super do |user|
+  #     if request.format.json?
+  #       data = {
+  #         token: user_authentication_token,
+  #         user_email: user.email
+  #       }
+  #       render json: data, status: 201 and return
+  #     end
+  #   end
+  # end
   def create
-    super do |user|
-      if request.format.json?
+    respond_to do |format|
+      format.html { super }
+      format.json do
+        self.resource = warden.authenticate!(auth_options)
+        sign_in(resource_name, resource)
         data = {
-          token: user_authentication_token,
-          user_email: user.email
+          user_token: self.resource.authentication_token,
+          user_email: self.resource.email
         }
-        render json: data, status: 201 and return
+        render json: data, status: 201
       end
     end
   end
