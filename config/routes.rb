3892128d62ef335983aa67d328b90dd35224465e
Victor Nițu <victor@thesponge.eu>
2015-03-05 22:04:43 +0200

    CSRF protection for Ember

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {sessions: 'sessions'}
   root 'ember#bootstrap'
   get '/*path' => 'ember#bootstrap'
+
+  namespace :api do
+    get :csrf, to: 'csrf#index'
+  end
+
 end
