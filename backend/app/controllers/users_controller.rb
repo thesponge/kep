c9b7bf74cc12472d30c20f3aec554e9b57108fc0
Victor Nițu <victor@thesponge.eu>
2015-03-10 15:42:21 +0200

    Added username field to user structure

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -71,6 +71,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
    def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :username, :password_confirmation)
   end
 end
