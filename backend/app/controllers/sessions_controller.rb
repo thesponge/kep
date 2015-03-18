c9fde9e7d7bcae1ebf73f9efcb72271276b5507f
Victor Nițu <victor@thesponge.eu>
2015-03-18 13:33:06 +0200

    Revert broken sessions_controller

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -2,11 +2,8 @@ class SessionsController < Devise::SessionsController
   respond_to :html, :json
 
   def create
-    respond_to do |format|
-#      format.html { super }
-      format.json do
-        self.resource = warden.authenticate!(auth_options)
-        sign_in(resource_name, resource)
+    super do |user|
+      if request.format.json?
         data = {
           token: user.authentication_token,
           email: user.email
