4721463942a5ae1552a0490ede2cbdc4e2d2d81a
georgiana-b <bere.georgi@gmail.com>
2015-03-18 22:47:03 +0200

    Solved login bug

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,23 +1,23 @@
 class ApplicationController < ActionController::API
   include ActionController::ImplicitRender  
+  include ActionController::HttpAuthentication::Token::ControllerMethods
   
-  
-#  before_filter :authenticate_user_from_token!
-#
-#  # Enter the normal Devise authentication path,
-#  # using the token authenticated user if available
-#  before_filter :authenticate_user!
-#
-#  private
-#
-#  def authenticate_user_from_token!
-#    authenticate_with_http_token do |token, options|
-#      user_email = options[:email].presence
-#      user = user_email && User.find_by_email(user_email)
-#
-#      if user && Devise.secure_compare(user.authentication_token, token)
-#        sign_in user, store: false
-#      end
-#    end
-#  end
+  before_filter :authenticate_user_from_token!
+
+  # Enter the normal Devise authentication path,
+  # using the token authenticated user if available
+  before_filter :authenticate_user!
+
+  private
+
+  def authenticate_user_from_token!
+    authenticate_with_http_token do |token, options|
+      user_email = options[:email].presence
+      user = user_email && User.find_by_email(user_email)
+
+      if user && Devise.secure_compare(user.authentication_token, token)
+        sign_in user, store: false
+      end
+    end
+  end
 end
