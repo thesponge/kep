c36f32949c184013c829189af3744c5a258e9bae
Victor Nițu <victor@thesponge.eu>
2015-03-19 22:42:58 +0200

    Fix request rendering in dashboard

--- frontend/app/routes/match/dashboard.js
+++ frontend/app/routes/match/dashboard.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
-import Request from "../../../models/request.js"
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   //model: function() {
   //  return Request.find();
   //}
+  model: function() {
+    return this.store.find('request');
+  }
 });
 
