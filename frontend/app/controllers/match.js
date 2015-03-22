2e5d925ef8b02046f2e543f0731096e00141c587
Victor Nițu <victor@thesponge.eu>
2015-03-22 23:34:24 +0200

    Register template, loaders, styling and stuff

--- frontend/app/controllers/match.js
+++ frontend/app/controllers/match.js
@@ -5,4 +5,15 @@ Ember.LinkView.reopen({
 });
 
 export default Ember.ObjectController.extend({
+  user: function() {
+    return this.store.deleteRecord('user');
+  }.property(),
+  actions: {
+    destroyCurrentUser: function() {
+      console.log('Accessing the destroy action');
+      this.get('model').destroyRecord().then(function() {
+        this.transitionToRoute('login');
+      }.bind(this));
+    },
+  }
 });
