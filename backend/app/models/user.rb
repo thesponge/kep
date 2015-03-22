6a47dffc2b70ef59ca53222f43b6dc7ddfeced34
georgiana-b <bere.georgi@gmail.com>
2015-03-22 13:12:53 +0200

    Fixed user CRUD

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
+  
   has_many :jobs
   has_many :requests
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, 
     before_save :ensure_authentication_token
   
   def ensure_authentication_token
