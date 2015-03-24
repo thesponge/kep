b6bd452122de01af6ea5e66d66671e7ee29660e7
Victor Nițu <victor@thesponge.eu>
2015-03-25 01:47:07 +0200

    Freezing the code, incoming frontend shitstorm

--- frontend/app/models/job.js
+++ frontend/app/models/job.js
@@ -11,8 +11,8 @@ var Job = DS.Model.extend({
   //job_type_ids:          attr(),
   //job_compensation_ids:  attr(),
   //job_priority_ids:      attr(),
-  types:                 DS.hasMany('jobType', {polymorphic: true, async: true, embedded: 'always'}),
-  compensations:         DS.hasMany('jobCompensation', {polymorphic: true, async: true, embedded: 'always'})
+  types:                 DS.hasMany('jobType', {async: true, embedded: 'always'}),
+  compensations:         DS.hasMany('jobCompensation', {async: true, embedded: 'always'})
 });
 
 //Job.reopenClass({
--- frontend/app/routes/jobs/edit.js
+++ frontend/app/routes/jobs/edit.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  beforeModel: function() {
+  beforeModel: function(){
     model.reload();
   }
 });
+
