6870eaf2cdf394c547b505a01c76b9aacc9a903f
Victor Nițu <victor@thesponge.eu>
2015-04-03 00:43:34 +0300

    Confirmation feedback. Fixes #29

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,17 +1,19 @@
 MyBackend::Application.routes.draw do
 #  get 'passwords_reset/new'
+  defaults format: "json" do
 
-  devise_for :users, controllers: { sessions: 'sessions' }
-  
-  namespace :api, :defaults => {:format => :json} do
-    namespace :v1 do
-      resources :users, :only => [:update, :destroy,:create, :show]
-      resources :jobs, :only => [:show, :index,:create, :update, :destroy]
-      resources :job_types
-      resources :requests, :only => [:show, :index]
-      resources :requests, :only => [:create, :update, :destroy]
-      resources :request_types
-      resources :matches
+    devise_for :users, controllers: { sessions: 'sessions', passwords: 'passwords', confirmations: 'confirmations' }
+    
+    namespace :api, :defaults => {:format => :json} do
+      namespace :v1 do
+        resources :users, :only => [:update, :destroy,:create, :show]
+        resources :jobs, :only => [:show, :index,:create, :update, :destroy]
+        resources :job_types
+        resources :requests, :only => [:show, :index]
+        resources :requests, :only => [:create, :update, :destroy]
+        resources :request_types
+        resources :matches
+      end
     end
   end
   
