6512b4bd87f3d098a60fb4982260e2fe893301e2
Victor Nițu <victor@thesponge.eu>
2015-04-03 01:58:29 +0300

    Reset password, completed. Fixes #32 for good.

--- backend/app/controllers/confirmations_controller.rb
+++ backend/app/controllers/confirmations_controller.rb
@@ -1,16 +1,5 @@
 class ConfirmationsController < Devise::ConfirmationsController
 
-  #def create
-  #  super do
-  #    if successfully_sent?(resource)
-  #      render status: 200, json: { success: true }
-  #    else
-  #      render status: 422, json: { errors: resource.errors.full_messages }
-  #    end
-  #    return
-  #  end
-  #end
-
   def show
     self.resource = resource_class.confirm_by_token(params[:confirmation_token])
 
--- backend/app/controllers/passwords_controller.rb
+++ backend/app/controllers/passwords_controller.rb
@@ -11,4 +11,13 @@ class PasswordsController < Devise::PasswordsController
     end
   end
 
+  def update
+    self.resource = resource_class.reset_password_by_token(resource_params)
+    if resource.errors.empty?
+      render status: 200, json: { success: true }
+    else
+      render status: 422, json: { errors: resource.errors.full_messages }
+    end
+  end
+
 end
