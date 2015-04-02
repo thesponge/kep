4bd088ed30e8edf3b5b0cc34265699a3de03bfa5
Victor Nițu <victor@thesponge.eu>
2015-04-02 16:49:04 +0300

    Match mini-templates, populated modal(s) and corresponding actions

--- frontend/app/routes/application.js
+++ frontend/app/routes/application.js
@@ -1,15 +1,20 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
+//import {ApplicationRouteMixin as SimpleAuthApplicationRouteMixin} from 'simple-auth/mixins/application-route-mixin';
+//import {ApplicationRouteMixin as EmberModalsApplicationRouteMixin} from 'ember-modals/mixins/routes/application';
+//
+//
+//export default Ember.Route.extend(SimpleAuthApplicationRouteMixin, EmberModalsApplicationRouteMixin, {
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
   actions: {
-      error: function(transition, error) {
-          this.notifications.addNotification({
-              message: error.errors[0],
-              type: 'error',
-              autoclear: false
-          });
-      }
+    error: function(transition, error) {
+        this.notifications.addNotification({
+            message: error.errors[0],
+            type: 'error',
+            autoclear: false
+        });
+    }
   }
 });
--- frontend/app/routes/apps.js
+++ frontend/app/routes/apps.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
+import ApplicationRouteMixin from 'ember-modals/mixins/routes/application';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+export default Ember.Route.extend(AuthenticatedRouteMixin, ApplicationRouteMixin, {
   model: function() {
     return Ember.RSVP.hash({
       user: this.store.find('user', { email: window.session.content.email })
