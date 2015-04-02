2159eb5c183ef083b84fa6c110cc71b5cee933ce
georgiana-b <bere.georgi@gmail.com>
2015-04-02 22:45:21 +0300

    Link changed for production

--- backend/app/mailers/match_mailer.rb
+++ backend/app/mailers/match_mailer.rb
@@ -2,7 +2,7 @@ class MatchMailer < ApplicationMailer
   
   def match_email(user,what,id)
     @user = user
-    @url  = 'http://localhost:4200/apps/match/dashboard/' + what + "/" + id.to_s 
+    @url  = 'http://kep.thesponge.eu/apps/match/dashboard/' + what + "/" + id.to_s 
     mail(to: @user.email, subject: 'Sample Email')
   end
   
