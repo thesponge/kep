f782370620bf3c46ec306e033ef2c2bb10fcfbad
georgiana-b <bere.georgi@gmail.com>
2015-04-02 18:25:05 +0300

    CRUD including relations

--- backend/app/models/request.rb
+++ backend/app/models/request.rb
@@ -4,6 +4,8 @@ class Request < ActiveRecord::Base
   has_and_belongs_to_many :request_types
   has_and_belongs_to_many :request_compensations
   has_and_belongs_to_many :request_priorities
+
+  accepts_nested_attributes_for :request_priorities
   
   validates :title, presence: true, length: { in: 5..150 }
   validates :description, presence: true, length: { in: 100..3000}
