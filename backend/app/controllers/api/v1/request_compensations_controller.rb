1962f642aedba38a9ded9613a8d634c1e469e536
georgiana-b <bere.georgi@gmail.com>
2015-04-03 18:22:33 +0300

    RequestCompensations Controller

---
+++ backend/app/controllers/api/v1/request_compensations_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::RequestCompensationsController < ApplicationController
+  
+  def index
+    render json: RequestCompensation.all
+  end
+
+  def show
+    render json: RequestCompensation.find(params[:id]) 
+  end
+  
+  protected
+  
+  def request_compensation_params
+    params.require(:request_compensation).permit(:compensation, requests_attributes: [:title, :description, :travel, :driver_license])
+  end
+  
+end
