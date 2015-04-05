ea8d7e7fc669c005a7cdd00a39368055390e0f3a
Victor Nițu <victor@thesponge.eu>
2015-04-05 17:26:22 +0300

    Update ember-cli to 0.2.2

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('application');
+  //this.route('application');
   //this.resource('jobs', function() {
   //  this.route('show', { path: ':job_id' });
   //  this.route('browse');
